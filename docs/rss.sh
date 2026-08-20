a2dd642862508e48c4a1929fe3429bde0c0541ab
pikapikapi <pikapikapi_kaori@icloud.com>
2026-08-20 08:32:46 +0800

    fix: remove style from rss

--- docs/rss.sh
+++ docs/rss.sh
@@ -18,7 +18,7 @@ urlencode() {
 }
 
 newest_files=$(
-    git ls-files -z '**/*.md' ':!:**/_*.md' ':!:**/README.md' ':!:**/About.md' ':!:**/Personal**.md' ':!:**/BriefComments.md' ':!:**/Beginning.md' ':!:**/Sites.md' ':!:pages/**/*.md' ':!:sources/**/*.md' |
+    git ls-files -z '**/*.md' ':!:**/_*.md' ':!:**/README.md' ':!:**/About.md' ':!:**/Personal**.md' ':!:**/BriefComments.md' ':!:**/Beginning.md' ':!:**/Sites.md' ':!:pages/**/*.md' ':!:sources/**/*.md' ':!:style/**/*.md' |
         xargs -0 -n1 -I{} -- git log -1 --format="%at {}" {} |
         sort -r |
         head -n10 |

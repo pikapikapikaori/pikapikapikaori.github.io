c7925fe4c32b2da72ffedbac296e3b09673f077a
pikapikapikaori <pikapikapi_kaori@icloud.com>
2026-09-12 00:11:33 +0800

    refactor: fix toc page date color 1) change toc page date color to --em-color; remove --global-background-blur 2) change --global-theme-color-blur to css calc to auto calculate; remove js setter

--- docs/utils/addWidgets.js
+++ docs/utils/addWidgets.js
@@ -206,13 +206,11 @@ function plugin(hook, vm) {
 
         colorPickerSlider.oninput = function () {
             document.documentElement.style.setProperty('--theme-color', hslToHex(this.value, 66, 78))
-            document.documentElement.style.setProperty('--global-theme-color-blur', hslToHex(this.value, 66, 78) + '1a')
         }
 
         Array.from(colorPickerPopupDiv.getElementsByClassName('color-picker-preset-color-btn-div')).forEach(colorPickerPresetColorBtn => {
             colorPickerPresetColorBtn.onclick = function (event) {
                 document.documentElement.style.setProperty('--theme-color', hslToHex(event.target.dataset.hue, 66, 78))
-                document.documentElement.style.setProperty('--global-theme-color-blur', hslToHex(event.target.dataset.hue, 66, 78) + '1a')
                 colorPickerSlider.value = event.target.dataset.hue
             }
         })
